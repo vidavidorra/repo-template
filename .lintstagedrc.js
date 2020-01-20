9ca416aaac4fe3130a5c6f2846532a948c0008e9
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-01-20 18:15:09 +0100

    fix: remove `git add` from lint-staged
    Since lint-staged v10, it automatically adds `git add` as final step.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': [
-    'npm run lint',
+  '*.{ts,tsx,js,jsx,json}': ['npm run lint', 'npm run format-file:default'],
+  '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format-file:default',
-    'git add',
-  ],
-  '*.{js,jsx,vue,ts,css,less,scss,html,htm,json,md,markdown,yml,yaml}': [
-    'npm run format-file:default',
-    'git add',
   ],
 };
