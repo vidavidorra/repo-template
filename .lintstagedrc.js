2ee691247b656456eb97436c257c46b9fb955818
Jeroen de Bruijn <vidavidorra@gmail.com>
2020-03-16 10:27:45 +0100

    feat: add lint workflows and update generic configs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': ['npm run lint', 'npm run format-file:default'],
+  '*.{ts,tsx,js,jsx,json}': [
+    'npm run lint-file',
+    'npm run format-file:default',
+  ],
   '*.{vue,ts,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format-file:default',
   ],
