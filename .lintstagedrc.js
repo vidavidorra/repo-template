09f5b5e0ed5b220b3fa06e796ce0b5da3a8f8764
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-08-01 16:58:01 +0200

    fix: format files during lint-staged for JS, TS and JSON files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx,json}': ['npm run lint-es:file:fix'],
+  '*.{ts,tsx,js,jsx,json}': ['npm run format:file', 'npm run lint-es:file:fix'],
   '*.{vue,css,less,scss,html,htm,md,markdown,yml,yaml}': [
     'npm run format:file',
   ],
