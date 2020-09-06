27477da61842ff3f8e805cc7bb07f6ba3fa1bd2f
Jeroen de Bruijn <vidavidorra+jdbruijn@gmail.com>
2020-09-06 09:44:36 +0200

    fix(lint): remove TypeScript linting and dependencies
    A project using this template isn't necessary written in TypeScript, so TypeScript linting and its dependencies.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 const defaultConfig = {
-  extends: [
-    'eslint:recommended',
-    'plugin:json/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: [],
   parserOptions: {
     sourceType: 'module',
@@ -18,22 +14,37 @@ const defaultConfig = {
   },
 };
 
+/**
+ * Add to `extends` of `defaultConfig`.
+ *
+ * The Prettier recommended configuration must be the last extension. See
+ * https://github.com/prettier/eslint-plugin-prettier#recommended-configuration.
+ *
+ * @param  {...configurations} configurations Configuration(s) to add.
+ * @return Superset of the `extends` with the given configuration(s) added.
+ */
+function addToDefaultExtends(...configurations) {
+  const prettierConfiguration = 'plugin:prettier/recommended';
+  const extendsSuperset = defaultConfig.extends
+    .concat(configurations)
+    .sort((a, b) => {
+      if (b === prettierConfiguration) {
+        return -1;
+      }
+      if (a === prettierConfiguration) {
+        return 1;
+      }
+      return 0;
+    });
+
+  return extendsSuperset;
+}
+
 const config = defaultConfig;
 config.overrides = [
   {
-    files: ['**/*.ts', '**/*.tsx'],
-    /**
-     * The Prettier recommended configuration must remain the configuration that
-     * is extended last. Therefore, this overrides `extends` completely.
-     */
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: defaultConfig.plugins.concat(['@typescript-eslint']),
+    files: ['**/*.json'],
+    extends: addToDefaultExtends('plugin:json/recommended'),
   },
 ];
 
